8ecdd152f23a1f33d2862278c4c9c5b273e07f2e
gitcrackerUK <pfsiwek@gmail.com>
2020-01-17 15:10:12 +0000

    event queue

--- main.js
+++ main.js
@@ -1,47 +1,13 @@
-var currentScope = 0; // global scope
-function a() {
-    var currentScope = 1,
-        one = 'scope1';
-    console.log(currentScope);
-b();
-    function b() {
-        var currentScope = 2,
-            two = 'scope2';
-        console.log(currentScope);
-c();
-        function c() {
-            var currentScope = 3,
-                three = 'scope3';
-            console.log(currentScope);
-            console.log(one +" "+ two +" "+ three); // climb up the scope chain to get one and two
-        };
-    };
-};
-a();
+function waitThreeSeconds() {
+    var ms = 3000 + new Date().getTime();
+    while (new Date() < ms) {}
+    console.log("finished function");
+}
+function clickHandler(){
+    console.log("click event!");
+}
 
-//
-// function b(){
-//     c();
-//     function c(){
-//         d();
-//         function d(){
-//
-//         }
-//     }
-//     // console.log(myVar);
-// }
-// function a(){
-//     var myVar = 2;
-//     // console.log(myVar);
-//     b();
-// }
-//
-// var myVar = 1;
-// // console.log(myVar);
-// a();
-// console.log(myVar);
+document.addEventListener("click", clickHandler());
 
-
-// var a ="Hello World";
-//
-// console.log(a);
+waitThreeSeconds();
+console.log("finished execution");
