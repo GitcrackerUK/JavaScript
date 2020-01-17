ded9136c26b35d04301e5c8e87aa5e83d62fe295
gitcrackerUK <pfsiwek@gmail.com>
2020-01-17 13:45:01 +0000

    Scope chain 2

--- main.js
+++ main.js
@@ -1,26 +1,44 @@
-
-
-
-
-function b(){
-    c();
-    function c(){
-        d();
-        function d(){
-            console.log(myVar);
-        }
-    }
-    // console.log(myVar);
-}
-function a(){
-    var myVar = 2;
-    // console.log(myVar);
-    b();
-}
-
-var myVar = 1;
-// console.log(myVar);
+var currentScope = 0; // global scope
+function a() {
+    var currentScope = 1,
+        one = 'scope1';
+    console.log(currentScope);
+b();
+    function b() {
+        var currentScope = 2,
+            two = 'scope2';
+        console.log(currentScope);
+c();
+        function c() {
+            var currentScope = 3,
+                three = 'scope3';
+            console.log(currentScope);
+            console.log(one +" "+ two +" "+ three); // climb up the scope chain to get one and two
+        };
+    };
+};
 a();
+
+//
+// function b(){
+//     c();
+//     function c(){
+//         d();
+//         function d(){
+//
+//         }
+//     }
+//     // console.log(myVar);
+// }
+// function a(){
+//     var myVar = 2;
+//     // console.log(myVar);
+//     b();
+// }
+//
+// var myVar = 1;
+// // console.log(myVar);
+// a();
 // console.log(myVar);
 
 
