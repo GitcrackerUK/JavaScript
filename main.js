de0015aaba3d56625431f8c332dae7a5ad651efa
gitcrackerUK <pfsiwek@gmail.com>
2020-01-18 12:00:52 +0000

    Boolean

---
+++ main.js
@@ -0,0 +1,74 @@
+//
+//
+//
+// var a="hi";
+// if (a){
+//
+//     console.log("Hello World")
+//
+// }
+// // console.log(a);
+// var a ;
+//
+// a= 3 == "3"
+// console.log(a);
+//
+// var b;
+//  b=3 ==="3"
+//  console.log(b);
+
+// var a = 0;
+// var b= false;
+// if (a==b ){
+//     console.log("They are equal!")
+// }
+//
+// else{
+//     console.log("They aren't!")
+// }
+//
+// if (a===b ){
+//     console.log("They are equal!")
+// }
+// else{
+//     console.log("They aren't!")
+// }
+// if (a!==b ){
+//     console.log("They are not equal!")
+// }
+// else{
+//     console.log("They aren't!")
+// }
+
+
+
+
+
+
+//
+//
+// console.log(Boolean(undefined)) // false
+// console.log(Boolean(null)) // false
+// console.log(Boolean("")) // false
+
+
+
+
+
+
+
+
+
+//
+
+
+var a;
+
+// goes out to internet and looks for a value
+
+if(a){
+    console.log("Something is there!!");
+}
+else {
+    console.log(" 'a' is undefined");
+}
