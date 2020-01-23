eb08fe4164274673a6790c3fa5a090fca46da2d3
gitcrackerUK <pfsiwek@gmail.com>
2020-01-23 20:45:04 +0000

    first, last, initial, rest

--- UnderscoreT/script1.js
+++ UnderscoreT/script1.js
@@ -13,6 +13,37 @@ window.addEventListener("load", function() {
     diff.innerHTML = _.difference(array, array2);
     without.innerHTML = _.without(array2, 'A','C');
 
+    // 2. first, last, initial, rest
+    var arr = [1, 2, 3, 4, 5, 6, 7, 8];
+
+    var first = document.getElementById('first');
+    var last = document.getElementById('last');
+    var initial = document.getElementById('initial');
+    var rest = document.getElementById('rest');
+
+    first.innerHTML = _.first(arr);
+    last.innerHTML = _.last(arr);
+    initial.innerHTML = _.initial(arr,3);
+    rest.innerHTML = _.rest(arr,3);
+
+    // 2. pick, omit, keys, values
+    var pick = document.getElementById('pick');
+    var omit = document.getElementById('omit');
+    var keys = document.getElementById('keys');
+    var values = document.getElementById('values');
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 });
